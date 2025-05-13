37f8378bd887f0018827f8441feec3590e3d2964
Jacob Quinn <quinn.jacobd@gmail.com>
2025-05-13 16:51:59 -0600

    Add some common utilities for working w/ aws libraries (#24)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,10 @@
 using Test, Aqua, LibAwsCommon
+import LibAwsCommon: FieldRef, StructRef, Future
+
+mutable struct Job
+    name::String
+    wage::Float64
+end
 
 @testset "LibAwsCommon" begin
     @testset "aqua" begin
@@ -17,5 +23,31 @@ using Test, Aqua, LibAwsCommon
             aws_logger_clean_up(logger)
             @test isfile(logpath) # might as well check this but we're mainly testing we don't crash
         end
+    end
+    @testset "FieldRef/StructRef" begin
+        job = Job("plumber", 50000.0)
+        GC.@preserve job begin
+            @test_throws AssertionError FieldRef(job, :name)
+            wage_field = FieldRef(job, :wage)
+            @test pointer(wage_field) isa Ptr{Float64}
+            @test unsafe_load(pointer(wage_field)) == 50000.0
+            @test_throws AssertionError StructRef(Ptr{Any}(pointer_from_objref(job)))
+            job_ref = StructRef(Ptr{Job}(pointer_from_objref(job)))
+            @test job_ref.wage == 50000.0
+        end
+    end
+    @testset "Future" begin
+        f = Future{Int}()
+        t = Threads.@spawn wait(f)
+        @test !istaskdone(t)
+        notify(f, 10)
+        @test fetch(t) == 10
+        @test wait(f) == 10
+        f2 = Future{Int}()
+        t2 = Threads.@spawn wait(f2)
+        @test !istaskdone(t2)
+        notify(f2, ArgumentError("Error!"))
+        @test_throws TaskFailedException fetch(t2)
+        @test_throws ArgumentError wait(f2)
     end
 end
